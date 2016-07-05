48d6c60c3ed7977faf5316393f63d273a66ee9fd
SusanZheng15 <zheng.susan12@gmail.com>
2016-07-05 15:47:33 -0400

    Done

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -1,6 +1,6 @@
 let name = "Audrey"
 name.uppercaseString
 
-let uppercaseName = name.uppercaseString
+let uppercaseName = name.uppercaseString    //uppercaseString is one of the methods that the String class have. Makes the string uppercase
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
